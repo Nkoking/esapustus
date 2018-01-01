bd918c845ce183dc2c0217530cc8aac36fed7859
Nkoking <nkonordlund@gmail.com>
2018-01-01 14:25:03 +0200

    Added collision and pausing

--- esapustus.py
+++ esapustus.py
@@ -27,11 +27,12 @@ class Esine(): # Parent class for all objects
         self.rect.y = self.y
 
 class ES(Esine): #Es class which extends esine class
-    speed = 7
-    width = 100
-
     def __init__(self):
+        self.speed = 7
+        self.width = 100
+        self.active = True
         self.resetPosition()
+        self.stop()
         Esine.__init__(self)
 
     def resetPosition(self):
@@ -45,16 +46,24 @@ class ES(Esine): #Es class which extends esine class
         self.x = random.randrange(0, display_width - self.width)
         self.y = -30
 
+    def stop(self):
+        if self.active:
+            self.speed = 7
+        else: self.speed = 0
+        self.active = not self.active 
+
 class Player(Esine):
     img = pygame.image.load('images/apustus.png') # Images
-    width = 100
     
     def __init__(self):
         self.x = (display_width * 0.45) # coordinates for player
         self.y = (display_height * 0.82)
+        self.width = 100
         self.points = 0
+        self.can_right = True
+        self.can_left = True
         Esine.__init__(self)
-
+    
 def start():
     game_loop(Player(), ES())
 
@@ -86,9 +95,8 @@ def playerCollidesWithES(player, es):
     return player.rect.colliderect(es.rect)
 
 def game_loop(player, es): # game
-    
+
     while True: # logic loop
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -97,10 +105,12 @@ def game_loop(player, es): # game
         pygame.event.pump()
         keys = pygame.key.get_pressed()
         
-        if keys[pygame.K_LEFT]: # recording keystrokes and moving
+        if keys[pygame.K_LEFT] and player.can_left: # recording keystrokes and moving
             player.move(dX=-10)
-        elif keys[pygame.K_RIGHT]:
+        elif keys[pygame.K_RIGHT] and player.can_right:
             player.move(dX=10)
+        elif keys[pygame.K_p]:
+            es.stop()
         elif keys[pygame.K_ESCAPE]:
             pygame.quit()
             quit()
@@ -112,15 +122,21 @@ def game_loop(player, es): # game
         gameDisplay.blit(es.img, (es.x, es.y))
 
         drawPoints(player)
+
+        if player.x < 0:
+            player.can_left = False
+        else: player.can_left = True
+        if player.x > (display_width - player.width):
+            player.can_right = False
+        else: player.can_right = True
             
         if playerCollidesWithES(player, es):
             player.points += es.point_value
             es.resetPosition()
 
         elif es.y > display_height:
-            crash("You didnt catch the ES in time. Hit any key to continue")
+            crash("You didnt catch the ES in time.")
             
-
         pygame.display.update() # updating screen
         clock.tick(60) # fps
 
